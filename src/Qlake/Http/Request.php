41b73c38d6c6f64a7d19c42de5499b48546058af
Reza Khodadadi <rezakho@gmail.com>
2015-03-03 00:47:24 +0330

    Added Request class docs

--- src/Qlake/Http/Request.php
+++ src/Qlake/Http/Request.php
@@ -7,18 +7,43 @@ use Qlake\Http\Header;
 class Request
 {
 
+	/**
+	 * An instance of Qlake\Http\Header.
+	 * 
+	 * @var Qlake\Http\Header
+	 */
 	protected $header;
 
 
+	/**
+	 * Current request method.
+	 * 
+	 * @var srting
+	 */
 	protected $method;
 
 
+	/**
+	 * Array of GET params.
+	 * 
+	 * @var array
+	 */
 	protected $query = [];
 
 
+	/**
+	 * Array of POST params.
+	 * 
+	 * @var array
+	 */
 	protected $data = [];
 
 
+	/**
+	 * Array of special params that use in framework core services.
+	 * 
+	 * @var array
+	 */
 	protected $specialInputs =
 	[
 		'__method' => 'GET',
@@ -27,15 +52,40 @@ class Request
 	];
 
 
+	/**
+	 * An instance of Qlake\Http\Environment.
+	 * 
+	 * @var Qlake\Http\Environment
+	 */
 	public $env;
 
 
+	/**
+	 * An instance of Qlake\Http\Cookie.
+	 * 
+	 * @var Qlake\Http\Cookie
+	 */
 	protected $cookie;
 
 
+	/**
+	 * An array of instances of Qlake\Http\File.
+	 * 
+	 * @var array
+	 */
 	protected $files;
 
 
+	/**
+	 * Constructor of Qlake\Http\Request class.
+	 * 
+	 * @param array $query 
+	 * @param array $data 
+	 * @param $server 
+	 * @param $cookies 
+	 * @param $files 
+	 * @param $content 
+	 */
 	public function __construct(array $query = [], array $data = [], $server = null, $cookies = [], $files = null, $content = null)
 	{
 		$this->header = new Header;
@@ -45,12 +95,23 @@ class Request
 	}
 
 
+	/**
+	 * Capture current request by current environment variables and return a new instance from Qlake\Http\Request class.
+	 * 
+	 * @return Qlake\Http\Request
+	 */
 	public static function capture()
 	{
 		return new static($_GET, $_POST);
 	}
 
 
+	/**
+	 * Parse and filter GET and POST params from special params.
+	 * 
+	 * @param array $inputs 
+	 * @return array
+	 */
 	protected function parseInputs(array $inputs)
 	{
 		$parsedInputs = [];
@@ -71,6 +132,11 @@ class Request
 	}
 
 
+	/**
+	 * Detect and return current request method.
+	 * 
+	 * @return string
+	 */
 	protected function detectMethod()
 	{
 		$method = $this->env['REQUEST_METHOD'];
@@ -79,48 +145,95 @@ class Request
 	}
 
 
+	/**
+	 * Return current request method.
+	 * 
+	 * @return string
+	 */
 	public function getMethod()
 	{
 		return $this->method = $this->method ?: $this->detectMethod();
 	}
 
 
+	/**
+	 * Return Header object from current request.
+	 * 
+	 * @return Qlake\Http\Header
+	 */
 	public function getHeader()
 	{
 		return $this->header;
 	}
 
 
+	/**
+	 * Return GET param by given name.
+	 * 
+	 * @param string $name
+	 * @param mixed $default
+	 * @return mixed
+	 */
 	public function getQuery($name, $default = null)
 	{
 		return $this->query[$name] ?: $default;
 	}
 
 
+	/**
+	 * Return all GET params as array.
+	 * 
+	 * @return array
+	 */
 	public function getAllQuery()
 	{
 		return $this->query;
 	}
 
 
+	/**
+	 * Return POST param by given name.
+	 * 
+	 * @param string $name 
+	 * @param mixed $default 
+	 * @return mixed
+	 */
 	public function getData($name, $default = null)
 	{
 		return $this->data[$name] ?: $default;
 	}
 
 
+	/**
+	 * Return all POST params as array.
+	 * 
+	 * @return array
+	 */
 	public function getAllData()
 	{
 		return $this->data;
 	}
 
 
+	/**
+	 * Return GET or POST param by given name.
+	 * 
+	 * @param string $name
+	 * @param mixed $default
+	 * @return mixed|null
+	 */
 	public function getInput($name, $default = null)
 	{
 		return $this->query[$name] ?: $this->data[$name] ?: $default;
 	}
 
 
+	/**
+	 * Return spacial input value like __method and __csrf.
+	 * 
+	 * @param string $name 
+	 * @return string|null
+	 */
 	public function getSpecialInput($name)
 	{
 		return $this->specialInputs[$name] ?: null;
