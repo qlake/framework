ce2247b88cd09ec29cd297319b228c9f657a0009
Reza Khodadadi <rezakho@gmail.com>
2015-03-06 18:07:23 +0330

    Edited Route unit test

--- tests/Routing/RouteTest.php
+++ tests/Routing/RouteTest.php
@@ -4,10 +4,30 @@ use Qlake\Routing\Route;
 
 class RouteTest extends PHPUnit_Framework_TestCase
 {
-	public function testSetNameKeyConfig()
+	public function testGetRouteMethods()
 	{
 		$route = new Route(['GET'], '/', null);
 
 		$this->assertEquals(['GET'], $route->getMethods());
+
+
+		$route = new Route(['GET', 'POST'], '/', null);
+
+		$this->assertEquals(['GET', 'POST'], $route->getMethods());
+
+
+		$route = new Route(['POST', 'GET'], '/', null);
+
+		$this->assertEquals(['POST', 'GET'], $route->getMethods());
+
+
+		$route = new Route(['GET', 'POST', 'PUT', 'PATCH', 'HEAD', 'DELETE', 'OPTIONS'], '/', null);
+
+		$this->assertEquals(['GET', 'POST', 'PUT', 'PATCH', 'HEAD', 'DELETE', 'OPTIONS'], $route->getMethods());
 	}
+
+
+	/*public function testGetRouteMethods()
+	{
+	}*/
 }
