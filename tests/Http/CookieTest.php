8ebbf9e2e134adc8c327f4ab7fbf6c9fcc9251ef
Reza Khodadadi <rezakho@gmail.com>
2015-03-19 00:42:56 +0330

    Added Http\Cookie tests

---
+++ tests/Http/CookieTest.php
@@ -0,0 +1,125 @@
+<?php
+
+use Qlake\Http\Cookie;
+
+class CookieTest extends PHPUnit_Framework_TestCase
+{
+	public function testCreateWithInvalidName()
+	{
+		$this->setExpectedException('InvalidArgumentException');
+
+		$cookie = new Cookie(false);
+	}
+
+
+
+	public function testCreateWithEmptyName()
+	{
+		$this->setExpectedException('InvalidArgumentException');
+
+		$cookie = new Cookie('');
+	}
+
+
+
+	public function testCreateWithName()
+	{
+		$cookie = new Cookie('name');
+
+		$this->assertEquals('name', $cookie->getName());
+	}
+
+
+
+	public function testCreateWithNameAndDefaultValue()
+	{
+		$cookie = new Cookie('name');
+
+		$this->assertEquals(null, $cookie->getValue());
+	}
+
+
+
+	public function testCreateWithNameAndValue()
+	{
+		$cookie = new Cookie('name', 'value');
+
+		$this->assertEquals('value', $cookie->getValue());
+	}
+
+
+
+	public function testCreateWithNameAndValueAndDefaultExpire()
+	{
+		$cookie = new Cookie('name', 'value');
+
+		$this->assertEquals(0, $cookie->getLifeTime());
+	}
+
+
+
+	public function testCreateWithNameAndValueAndExpire()
+	{
+		$cookie = new Cookie('name', 'value', 10);
+
+		$this->assertEquals(10, $cookie->getLifeTime());
+	}
+
+
+
+	public function testCreateWithNameAndValueAndExpireByUTCTime()
+	{
+		$time =  time() + 10;
+
+		$cookie = new Cookie('name', 'value', $time);
+
+		$this->assertEquals($time, $cookie->getLifeTime());
+	}
+
+
+
+	public function testCreateWithDefaultPath()
+	{
+		$cookie = new Cookie('name', 'value', 0);
+
+		$this->assertEquals('/', $cookie->getPath());
+	}
+
+
+
+	public function testCreateWithNullPath()
+	{
+		$cookie = new Cookie('name', 'value', 0, null);
+
+		$this->assertEquals('/', $cookie->getPath());
+	}
+
+
+
+	public function testCreateWithGivenPath()
+	{
+		$cookie = new Cookie('name', 'value', 0, '/path');
+
+		$this->assertEquals('/path', $cookie->getPath());
+	}
+
+
+
+	public function testCreateWithDefaultDomain()
+	{
+		$cookie = new Cookie('name', 'value', 0, null);
+
+		$this->assertEquals(null, $cookie->getDomain());
+	}
+
+
+
+	public function testCreateWithGivenDomain()
+	{
+		$cookie = new Cookie('name', 'value', 0, '/path', 'domain.com');
+
+		$this->assertEquals('domain.com', $cookie->getDomain());
+	}
+
+
+}
